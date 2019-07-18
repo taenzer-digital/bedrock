7e530cd0c81f61d9bf7a48317423275058d586bd
Daniel Crump <d.crump@taenzer.me>
2019-07-18 21:49:39 +0200

    transpile to ES5

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,49 @@
 import { eslint } from 'rollup-plugin-eslint';
-import { default as prettier } from 'rollup-plugin-prettier';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import prettier from 'rollup-plugin-prettier';
+import filesize from 'rollup-plugin-filesize';
+import progress from 'rollup-plugin-progress';
 
 const prettierConfig = {
-  'singleQuote': true,
+  singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'always',
 };
+
+const pluginOptions = [
+  uglify(),
+  prettier(prettierConfig),
+  commonjs(),
+  eslint({
+    fix: true,
+  }),
+  progress(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  filesize({
+    showGzippedSize: false,
+  }),
+];
 
 module.exports = [
   {
     input: 'src/js/bedrock.js',
     output: {
+      name: 'docs',
       file: 'js/bundle.js',
       format: 'cjs',
     },
-    plugins: [
-      eslint({
-        fix: true,
-      }),
-      prettier(prettierConfig),
-    ],
+    plugins: pluginOptions,
   },
   {
     input: 'src/js/index.js',
     output: {
+      name: 'main',
       file: 'js/index.js',
       format: 'cjs',
     },
-    plugins: [
-      eslint({
-        fix: true,
-      }),
-      prettier(prettierConfig),
-    ],
+    plugins: pluginOptions,
   },
 ];
